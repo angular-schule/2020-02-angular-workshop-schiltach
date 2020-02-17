da0f313a3b6a820ccddf94980d638bc60ebba49a
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2020-02-17 13:15:56 +0100

    typescript-playground

---
+++ typescript-playground/test.ts
@@ -0,0 +1,7 @@
+export class Test {
+    constructor(private antwort: number, private test = 'blubb') {
+        console.log('Hallo Welt! 😊', this.antwort)
+    }
+}
+
+export const blubb = 'Blubb';
