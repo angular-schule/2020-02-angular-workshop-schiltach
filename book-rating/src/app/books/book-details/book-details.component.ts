03a91d88d41b6cf8b1e0f8b1d219d8118a0919f5
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2020-02-20 13:47:13 +0100

    rxjs: error handling

--- book-rating/src/app/books/book-details/book-details.component.ts
+++ book-rating/src/app/books/book-details/book-details.component.ts
@@ -2,25 +2,27 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { BookStoreService } from '../shared/book-store.service';
 import { Book } from '../shared/book';
-import { map, mergeMap, switchMap } from 'rxjs/operators';
+import { map, mergeMap, switchMap, tap, catchError, share } from 'rxjs/operators';
+import { of } from 'rxjs';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'br-book-details',
   templateUrl: './book-details.component.html',
   styleUrls: ['./book-details.component.scss']
 })
-export class BookDetailsComponent implements OnInit {
+export class BookDetailsComponent {
 
-  book: Book;
+  book$ = this.route.paramMap.pipe(
+    map(p => p.get('isbn')),
+    switchMap(isbn => this.bs.getSingleBook(isbn).pipe(
+      catchError((e: HttpErrorResponse) => of({
+        isbn: 'FEHLER',
+        description: 'Error loadig' + e.url
+      }))
+    ))
+  );
 
   constructor(private route: ActivatedRoute, private bs: BookStoreService) { }
-
-  ngOnInit() {
-    this.route.paramMap.pipe(
-      map(p => p.get('isbn')),
-      switchMap(isbn => this.bs.getSingleBook(isbn))
-    )
-    .subscribe(book => this.book = book);
-  }
 
 }
